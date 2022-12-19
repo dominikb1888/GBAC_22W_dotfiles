2cee037b5b161df0b48e0ccbd380a04f09728e02
Dominik Boehler <dominik.boehler@gmx.net>
2022-12-19 12:48:05 +0100

    added theme and myfunctions

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ sudo apt-get update && sudo apt-get install fzf
 
 zshrc() {
     echo "==========================================================="
+    echo "             cloning powerlevel10k theme               "
+    echo "-----------------------------------------------------------"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    echo "==========================================================="
     echo "             cloning zsh-autosuggestions                   "
     echo "-----------------------------------------------------------"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -16,4 +20,7 @@ zshrc() {
     cat .zshrc > $HOME/.zshrc
 }
 
+cat myfunction.sh > $HOME/myfunctions.sh
+
 zshrc
+exec zsh
