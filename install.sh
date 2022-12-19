1b1433d9885559f500ea507a15b67170152db554
Dominik Boehler <dominik.boehler@gmx.net>
2022-12-19 12:41:13 +0100

    added linking of zshrc

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install fzf
+
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "                 cloning zsh-zsh-nvm                       "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
+}
+
+zshrc
